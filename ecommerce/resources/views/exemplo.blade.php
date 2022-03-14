b6ce6567d69fd06e7926a5f4c51fe89765cdbf93
joaovictor91 <j-victor-franco@live.com>
2022-03-14 17:02:35 -0300

    Ecommerce

---
+++ ecommerce/resources/views/exemplo.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('titulo')
+
+    Esta é uma página de exemplo!
+@endsection
+
+@section('conteudo')
+
+    <h3>Este é um exemplo de conteudo!</h3>
+    <div class="alert alert-danger">Conteúdo do Alert!</div>
+@endsection
+
+
